56fb7ab09ac6f944e09018b736ffd858b4cda096
MrSinaMaleki <sina67631@gmail.com>
2024-09-07 19:11:27 +0330

    fetching all of posts

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,8 @@ from blog import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+
+    path('posts/', views.all_posts, name='all_posts'),
     path('see_post/<int:pk>', views.one_post, name='one_post'),
     path('create_post', views.add_new_post, name='add_new_post'),
 
