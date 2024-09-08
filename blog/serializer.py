f425a73273050b3e88a06f7159c0ba4b99256ec0
MrSinaMaleki <sina67631@gmail.com>
2024-09-08 14:49:12 +0330

    last serilazer updates!

--- blog/serializer.py
+++ blog/serializer.py
@@ -1,15 +1,21 @@
 from rest_framework import serializers
-from blog.models import Post,Author
+from blog.models import Post, Author
 
 
 class PostSerializer(serializers.ModelSerializer):
+    # TO Do : What are these ?!
+    author_name = serializers.SerializerMethodField()
     author = serializers.PrimaryKeyRelatedField(queryset=Author.objects.all())
+
     class Meta:
         model = Post
-        fields = ['title', 'description', 'author', 'created_at', 'updated_at']
+        fields = ['title', 'description', 'author', 'author_name', 'created_at', 'updated_at']
+
+    def get_author_name(self, obj):
+        return obj.author.name  # Adjust based on the field you need
 
 
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
-        fields = ['id','name', 'bio', 'email', 'created_at']
+        fields = ['id', 'name', 'bio', 'email', 'created_at']
